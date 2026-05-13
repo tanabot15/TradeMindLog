cb38ee02b32a52ecb5d6aa2b1ea410d80bc0f880
tanabot15 <kenichirosuzuki1@gmail.com>
2026-05-13 21:13:11 +0900

    Initial Commit

---
+++ TradeMindLog/TradeMindLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeMindLogApp.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/05/13.
+//
+
+import SwiftUI
+
+@main
+struct TradeMindLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
