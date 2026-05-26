1c8eb3588d4d4fc368cbbacc21aef29eb10eb662
tanabot15 <kenichirosuzuki1@gmail.com>
2026-05-26 12:38:23 +0900

    Add Evaluation View

---
+++ TradeMindLog/View/EvaluationView.swift
@@ -0,0 +1,18 @@
+//
+//  EvaluationView.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/05/26.
+//
+
+import SwiftUI
+
+struct EvaluationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EvaluationView()
+}
