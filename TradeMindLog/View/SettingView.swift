637381fa5d14fc3a2c18dd976236694757551625
tanabot15 <kenichirosuzuki1@gmail.com>
2026-06-01 08:16:46 +0900

    Version up

--- TradeMindLog/View/SettingView.swift
+++ TradeMindLog/View/SettingView.swift
@@ -203,7 +203,7 @@ private extension SettingView {
                 Text("Version")
                 Spacer()
                 // change when updating
-                Text("2.5")
+                Text("2.6")
                     .foregroundColor(.secondary)
             }
             
