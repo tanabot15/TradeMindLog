9364df1946df1e3eced27a117cb53bd614c3f0a1
tanabot15 <kenichirosuzuki1@gmail.com>
2026-05-31 15:51:07 +0900

    Refactering, Add badge to List tab

---
+++ TradeMindLog/View/EmptyStateView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateView.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/05/29.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateView()
+}
