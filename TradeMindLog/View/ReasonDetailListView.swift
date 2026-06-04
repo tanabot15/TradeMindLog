6820369d04dc453802d9cea51b427895ec329616
tanabot15 <kenichirosuzuki1@gmail.com>
2026-06-04 17:09:56 +0900

    Create ReasonDetailListView

---
+++ TradeMindLog/View/ReasonDetailListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReasonDetailListView.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/06/04.
+//
+
+import SwiftUI
+
+struct ReasonDetailListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReasonDetailListView()
+}
