199353e0e5df1c470a10e1f059e6113ed1b27fff
tanabot15 <kenichirosuzuki1@gmail.com>
2026-06-05 09:21:23 +0900

    Improve recordListView by creating ReasonTagsView

---
+++ TradeMindLog/View/ReasonTagsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReasonTagsView.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/06/05.
+//
+
+import SwiftUI
+
+struct ReasonTagsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReasonTagsView()
+}
