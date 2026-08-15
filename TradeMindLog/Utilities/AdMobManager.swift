2e063f2007acadce1068c47454594d53f04d3b17
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-15 13:25:21 +0900

    Add Google AdMob

---
+++ TradeMindLog/Utilities/AdMobManager.swift
@@ -0,0 +1,8 @@
+//
+//  AdMobManager.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/08/15.
+//
+
+import Foundation
---
+++ TradeMindLog/View/Component/BannerAdView.swift
@@ -0,0 +1,18 @@
+//
+//  BannerAdView.swift
+//  TradeMindLog
+//
+//  Created by Kenichiro Suzuki on 2026/08/15.
+//
+
+import SwiftUI
+
+struct BannerAdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BannerAdView()
+}
